580c5690999220fa5b92350ac986d3e7aedb8191
Cayo <cayohollanda@hotmail.com>
2018-07-30 20:19:40 -0300

    PUT, DELETE, e GET para Cliente

--- src/main/java/br/com/cayohollanda/cursomc/resources/ClienteResource.java
+++ src/main/java/br/com/cayohollanda/cursomc/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package br.com.cayohollanda.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cayohollanda.cursomc.domain.Cliente;
+import br.com.cayohollanda.cursomc.dto.ClienteDTO;
 import br.com.cayohollanda.cursomc.services.ClienteService;
 
 @RestController
@@ -15,13 +24,54 @@ import br.com.cayohollanda.cursomc.services.ClienteService;
 public class ClienteResource {
 
 	@Autowired
-	private ClienteService clienteService;
+	private ClienteService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-		Cliente obj = this.clienteService.find(id);
+		Cliente obj = this.service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = this.service.fromDto(objDto);
+		obj.setId(id);
+		obj = this.service.update(obj);
+
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = this.service.findAll();
+		// Fazendo map de Cliente para ClienteDTO, igual ao map do Angular
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	// Método que faz a paginação, recebendo página, linhas por página,
+	// ordenação e direção, fazendo o Map também para ClienteDTO,
+	// mas não precisa fazer aquele map maior por que o Page já
+	// disponibiliza por ser tecnologia mais avançada
+	@RequestMapping(value="/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> list = this.service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
--- src/main/java/br/com/cayohollanda/cursomc/services/ClienteService.java
+++ src/main/java/br/com/cayohollanda/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.cayohollanda.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.cayohollanda.cursomc.domain.Cliente;
+import br.com.cayohollanda.cursomc.dto.ClienteDTO;
 import br.com.cayohollanda.cursomc.repositories.ClienteRepository;
+import br.com.cayohollanda.cursomc.services.exceptions.DataIntegrityException;
 import br.com.cayohollanda.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,6 +25,44 @@ public class ClienteService {
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = this.repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return this.repo.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = this.find(obj.getId());
+		this.updateData(newObj, obj);
+		return this.repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		this.find(id);
+		try {			
+			this.repo.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll() {
+		return this.repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest	= PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return this.repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDto(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 	
 }
