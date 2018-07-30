d484728eaae621a0844e8de5204873215532b086
Cayo <cayohollanda@hotmail.com>
2018-07-30 15:56:16 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/br/com/cayohollanda/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/cayohollanda/cursomc/services/CategoriaService.java
@@ -21,4 +21,9 @@ public class CategoriaService {
 				+ ", Tipo: " + Categoria.class.getName()));
 	}
 	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return this.repo.save(obj);
+	}
+	
 }
