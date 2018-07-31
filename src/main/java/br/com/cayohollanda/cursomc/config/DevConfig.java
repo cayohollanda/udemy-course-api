33187ede3a75353e6d60346bac6776c8ea12dab9
Cayo Hollanda <cayohollanda@hotmail.com>
2018-07-31 20:32:15 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/cayohollanda/cursomc/config/DevConfig.java
@@ -0,0 +1,34 @@
+package br.com.cayohollanda.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.cayohollanda.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if(!"create".equals(this.strategy)) {
+			return false;
+		}
+		
+		this.dbService.instantiateDatabase();
+		
+		return true;
+	}
+	
+}
