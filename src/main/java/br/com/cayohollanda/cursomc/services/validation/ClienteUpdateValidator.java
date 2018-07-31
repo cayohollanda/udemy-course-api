9c1f025b7e98be12569a1ff26a19acaf94a54225
Cayo Hollanda <cayohollanda@hotmail.com>
2018-07-31 00:23:52 -0300

    Validação customizada: email não repetido na atualização de Cliente

---
+++ src/main/java/br/com/cayohollanda/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package br.com.cayohollanda.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import br.com.cayohollanda.cursomc.domain.Cliente;
+import br.com.cayohollanda.cursomc.dto.ClienteDTO;
+import br.com.cayohollanda.cursomc.repositories.ClienteRepository;
+import br.com.cayohollanda.cursomc.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) { }
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente cli = this.repo.findByEmail(objDto.getEmail());
+		
+		if(cli != null && !cli.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "E-mail já cadastrado na base de dados"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+	
+}
