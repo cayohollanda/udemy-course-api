30229d5ef070c760bdf3327dd9b26d47993c1f3c
Cayo Hollanda <cayohollanda@hotmail.com>
2018-07-30 23:29:26 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/br/com/cayohollanda/cursomc/services/ClienteService.java
+++ src/main/java/br/com/cayohollanda/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package br.com.cayohollanda.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.cayohollanda.cursomc.domain.Cidade;
 import br.com.cayohollanda.cursomc.domain.Cliente;
+import br.com.cayohollanda.cursomc.domain.Endereco;
+import br.com.cayohollanda.cursomc.domain.enums.TipoCliente;
 import br.com.cayohollanda.cursomc.dto.ClienteDTO;
+import br.com.cayohollanda.cursomc.dto.ClienteNewDTO;
 import br.com.cayohollanda.cursomc.repositories.ClienteRepository;
+import br.com.cayohollanda.cursomc.repositories.EnderecoRepository;
 import br.com.cayohollanda.cursomc.services.exceptions.DataIntegrityException;
 import br.com.cayohollanda.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,14 +29,20 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = this.repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
 	}
-	
+
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return this.repo.save(obj);
+		obj = this.repo.save(obj);
+		this.enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -37,32 +50,52 @@ public class ClienteService {
 		this.updateData(newObj, obj);
 		return this.repo.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
 		this.find(id);
-		try {			
+		try {
 			this.repo.deleteById(id);
-		} catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
 		}
 	}
-	
+
 	public List<Cliente> findAll() {
 		return this.repo.findAll();
 	}
-	
+
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		PageRequest pageRequest	= PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return this.repo.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromDto(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
-	
+
+	public Cliente fromDto(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+
+		return cli;
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
+
 }
