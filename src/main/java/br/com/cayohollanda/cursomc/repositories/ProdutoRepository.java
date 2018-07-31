9d71b09245ddde2d1cce00098291ebb3a7bde86e
Cayo <cayohollanda@hotmail.com>
2018-07-31 19:06:07 -0300

    Busca de pedidos por nome e categorias

--- src/main/java/br/com/cayohollanda/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/br/com/cayohollanda/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,29 @@
 package br.com.cayohollanda.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.cayohollanda.cursomc.domain.Categoria;
 import br.com.cayohollanda.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);	
+
+	/*
+	Solução com JPQL, colocando @Param nos parâmetros do JPQL, nesse caso usarei direto com o Spring Data JPA
+	
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	
+	*/
 }
