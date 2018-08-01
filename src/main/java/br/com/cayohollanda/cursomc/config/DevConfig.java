b764059539ec871a2a4df13a095a899dfe5ddbb7
Cayo Hollanda <cayohollanda@hotmail.com>
2018-07-31 22:24:06 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/cayohollanda/cursomc/config/DevConfig.java
+++ src/main/java/br/com/cayohollanda/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.cayohollanda.cursomc.services.DBService;
+import br.com.cayohollanda.cursomc.services.EmailService;
+import br.com.cayohollanda.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		this.dbService.instantiateDatabase();
 		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 	
 }
