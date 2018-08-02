b407b59d4788a5626cf92af8d9067b5d9e61546a
Cayo Hollanda <cayohollanda@hotmail.com>
2018-08-02 00:39:34 -0300

    Esqueci a senha

--- src/main/java/br/com/cayohollanda/cursomc/services/AbstractEmailService.java
+++ src/main/java/br/com/cayohollanda/cursomc/services/AbstractEmailService.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 
+import br.com.cayohollanda.cursomc.domain.Cliente;
 import br.com.cayohollanda.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -32,6 +33,24 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setSubject("Pedido confirmado! Código: " + obj.getId());
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText(obj.toString());
+		
+		return sm;
+	}
+	
+	@Override
+	public void sendNewPassword(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = this.prepareNewPasswordEmail(cliente, newPass);
+		this.sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(this.sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
 		
 		return sm;
 	}
--- src/main/java/br/com/cayohollanda/cursomc/services/EmailService.java
+++ src/main/java/br/com/cayohollanda/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import br.com.cayohollanda.cursomc.domain.Cliente;
 import br.com.cayohollanda.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -15,5 +16,7 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPassword(Cliente cliente, String newPass);
 	
 }
