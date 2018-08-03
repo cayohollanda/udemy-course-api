e646421ab520a9ce2706c368bdea477c89bd3f97
Cayo <cayohollanda@hotmail.com>
2018-08-03 20:45:12 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/br/com/cayohollanda/cursomc/CursomcApplication.java
+++ src/main/java/br/com/cayohollanda/cursomc/CursomcApplication.java
@@ -1,13 +1,19 @@
 package br.com.cayohollanda.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.cayohollanda.cursomc.services.S3Service;
 
 // Implementando CommandLineRunner para instanciação ao rodar aplicação.
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -15,6 +21,8 @@ public class CursomcApplication implements CommandLineRunner {
 
 	// Método que faz a instanciação ao rodar a aplicação
 	@Override
-	public void run(String... args) throws Exception { }
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\Koala");
+	}
 	
 }
